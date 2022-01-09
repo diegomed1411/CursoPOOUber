cc71c649e9a09c12f0d67d76486eed293c8e2e2b
diegomed1411 <37563670+diegomed1411@users.noreply.github.com>
2022-01-09 19:28:17 -0300

    extendiendo clases cars

---
+++ Java/UberX.java
@@ -0,0 +1,10 @@
+class UberX extends Car {
+    String brand;
+    String model;
+
+    public UberX(String licence, Account driver){
+        super(licence, driver);
+        this.brand = brand;
+        this.model = model;
+    }
+}
