7f06b4cab4d0c2085241ca4acbeba1b4196c6a72
diegomed1411 <37563670+diegomed1411@users.noreply.github.com>
2022-01-14 09:04:28 -0300

    polimorfismo y encapsulamiento

--- Java/main.java
+++ Java/main.java
@@ -1,16 +1,16 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
-        car.passegenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Andrea Herrera";
+        UberVan uberVan = new UberVan("FGH345", new Account("Andres Herrera", "AND123"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
         car2.passegenger = 3;
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }
